b174e3cbc8cf348bfd9be8cc2758705e9035a2cf
Owen Gretzinger <owengretzinger@gmail.com>
2024-05-17 08:41:06 -0400

    Initialize new project (#2)

--- src/app/error.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import NotFound from "./not-found";
-
-export default function Error() {
-  return NotFound();
-}
--- src/lib/clickScroll.tsx
+++
@@ -1,8 +0,0 @@
-export default function handleClickScroll(section: string) {
-  const yOffset = -60;
-  const element = document.getElementById(section);
-  if (element) {
-    const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
-    window.scrollTo({ top: y, behavior: "smooth" });
-  }
-}
