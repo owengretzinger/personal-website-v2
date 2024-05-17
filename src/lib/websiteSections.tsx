2c4815f892ebffb84beb111e8a5f8b2f3cec2c7c
owengretzinger <owengretzinger@gmail.com>
2024-05-17 08:39:20 -0400

    Initialize new project

--- src/lib/websiteSections.tsx
+++
@@ -1,7 +0,0 @@
-export default function websiteSections(lowerCase: boolean) {
-  if (lowerCase) {
-    return ["about", "projects", "articles", "contact"];
-  } else {
-    return ["About", "Projects", "Articles", "Contact"];
-  }
-}
