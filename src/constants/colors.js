8f2cc5b9f4c874c2c421c864dc2d45ccc8440f19
owengretzinger <owengretzinger@gmail.com>
2024-05-22 20:19:14 -0400

    Set up tailwind colors & install prettier modules

---
+++ src/constants/colors.js
@@ -0,0 +1,13 @@
+// const twColors = require("tailwindcss/colors");
+
+module.exports = {
+  transparent: "transparent",
+  current: "currentColor",
+  black: "#1C2024",
+  grey: "#5F616C",
+  orange: "#EF5922",
+  lightGrey: "#DADADA",
+  darkTeal: "#008573",
+  lightTeal: "#CCF3EA",
+  white: "#FFFFFF",
+};
