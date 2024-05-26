142a6311ddbda08d01d804960f573333459f32ee
Owen Gretzinger <owengretzinger@gmail.com>
2024-05-26 08:55:31 -0400

    Dark mode (#5)

---
+++ src/components/LightDarkModeToggle.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { IconMoon, IconSun } from "@tabler/icons-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function LightDarkModeToggle() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme, systemTheme } = useTheme();
+  const actualTheme = theme === "system" || !theme ? systemTheme : theme;
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <button
+      onClick={() => setTheme(actualTheme === "light" ? "dark" : "light")}
+      className="p-4"
+    >
+      {actualTheme === "dark" ? (
+        <IconSun size={32} stroke={1.5} className="text-black" />
+      ) : (
+        <IconMoon size={32} stroke={1.5} className="text-black" />
+      )}
+    </button>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,24 @@
-const colors = require("./src/constants/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    colors: colors,
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "rgb(var(--color-black) / <alpha-value>)",
+      grey: "rgb(var(--color-grey) / <alpha-value>)",
+      orange: "rgb(var(--color-orange) / <alpha-value>)",
+      lightGrey: "rgb(var(--color-lightGrey) / <alpha-value>)",
+      darkTeal: "rgb(var(--color-darkTeal) / <alpha-value>)",
+      lightTeal: "rgb(var(--color-lightTeal) / <alpha-value>)",
+      white: "rgb(var(--color-white) / <alpha-value>)",
+    },
+    fontFamily: {
+      serif: ['"Crimson Pro"', ...defaultTheme.fontFamily.serif],
+      sans: ['"Segoe UI"', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
