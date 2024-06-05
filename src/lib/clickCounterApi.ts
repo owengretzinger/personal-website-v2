8e5ac60b2173312b6351aeacae5d87ddb46ba29c
owengretzinger <owengretzinger@gmail.com>
2024-06-05 07:28:27 -0400

    Add try catch logic back to click counter

--- src/lib/clickCounterApi.ts
+++ src/lib/clickCounterApi.ts
@@ -1,14 +1,18 @@
 "use server";
 
 export async function fetchCounterClicks() {
-  const response = await fetch(
-    `https://keyvalue.immanuel.co/api/KeyVal/GetValue/${process.env.CLICKCOUNTKEY}/count`,
-    {
-      cache: "no-store",
-    },
-  );
-  const data = await response.json();
-  return parseInt(data);
+  try {
+    const response = await fetch(
+      `https://keyvalue.immanuel.co/api/KeyVal/GetValue/${process.env.CLICKCOUNTKEY}/count`,
+      {
+        cache: "no-store",
+      },
+    );
+    const data = await response.json();
+    return parseInt(data);
+  } catch {
+    return 0;
+  }
 }
 
 export async function incrementGlobalCounterClicks(amount: number) {
