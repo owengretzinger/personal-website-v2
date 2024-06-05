af5e670d73e6dc1075086393f1badc0ca695b75a
owengretzinger <owengretzinger@gmail.com>
2024-06-05 07:21:38 -0400

    Fix Next.js error to do with try/catch block

--- src/lib/clickCounterApi.ts
+++ src/lib/clickCounterApi.ts
@@ -1,29 +1,23 @@
 "use server";
 
 export async function fetchCounterClicks() {
-  try {
-    const response = await fetch(
-      `https://keyvalue.immanuel.co/api/KeyVal/GetValue/${process.env.CLICKCOUNTKEY}/count`,
-      {
-        cache: "no-store",
-      },
-    );
-    const data = await response.json();
-    return parseInt(data);
-  } catch (error) {
-    console.error(error);
-    return 0;
-  }
+  const response = await fetch(
+    `https://keyvalue.immanuel.co/api/KeyVal/GetValue/${process.env.CLICKCOUNTKEY}/count`,
+    {
+      cache: "no-store",
+    },
+  );
+  const data = await response.json();
+  return parseInt(data);
 }
 
 export async function incrementGlobalCounterClicks(amount: number) {
-  try {
-    const clickCount = await fetchCounterClicks();
-    await fetch(
-      `https://keyvalue.immanuel.co/api/KeyVal/UpdateValue/${process.env.CLICKCOUNTKEY}/count/${clickCount + amount}`,
-      { method: "POST" },
-    );
-  } catch (error) {
-    console.error(error);
+  const clickCount = await fetchCounterClicks();
+  if (typeof clickCount !== "number" || clickCount <= 0) {
+    return;
   }
+  await fetch(
+    `https://keyvalue.immanuel.co/api/KeyVal/UpdateValue/${process.env.CLICKCOUNTKEY}/count/${clickCount + amount}`,
+    { method: "POST" },
+  );
 }
